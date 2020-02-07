64a623559ea96a384366aa1eb0f73a65221518f4
Lydia <lydiagashawtena@gmail.com>
2020-02-07 18:12:03 +0300

    Add Initial UI

--- resources/js/layouts/AppFooter.vue
+++ resources/js/layouts/AppFooter.vue
@@ -1,107 +1,105 @@
 <template>
-  <footer class="footer has-cards">
-    <div class="container container-lg">
-      <div class="row">
-        <div class="col-md-6 mb-5 mb-md-0">
-          <div class="card card-lift--hover shadow border-0">
-            <router-link title="Landing Page" to="/landing">
-              <img class="card-img" v-lazy="'img/theme/landing.jpg'">
-            </router-link>
-          </div>
+    <footer class="footer has-cards">
+        <!-- <div class="container container-lg">
+            <div class="row">
+                <div class="col-md-6 mb-5 mb-md-0">
+                    <div class="card card-lift--hover shadow border-0">
+                        <router-link to="/landing" title="Landing Page">
+                            <img v-lazy="'img/theme/landing.jpg'" class="card-img">
+                        </router-link>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-5 mb-lg-0">
+                    <div class="card card-lift--hover shadow border-0">
+                        <router-link to="/profile" title="Profile Page">
+                            <img v-lazy="'img/theme/profile.jpg'" class="card-img">
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="container container-lg">
+            <div class="row row-grid align-items-center">
+                <div class="col-lg-7">
+                    <h3 class="text-primary font-weight-light mb-2">Address</h3>
+                    <h4 class="mb-0 font-weight-light">STEMpower Inc.</h4>
+
+                    <h4 class="mb-0 font-weight-light">81 Washington Street, Suite 308</h4>
+
+                    <h4 class="mb-0 font-weight-light"> Salem, MA, 01970</h4>
+                    <h4 class="mb-0 font-weight-light"> U.S.A</h4>
+                </div>
+                <!-- <div class="col-lg-4">
+                    <h3 class="text-primary font-weight-light mb-2">Contact us</h3>
+                    <h4 class="mb-0 font-weight-light">+ 1 703-463-4468</h4>
+
+                    <h4 class="mb-0 font-weight-light"> info@stempower.org</h4>
+                </div> -->
+                <div class="col-lg-5 text-lg-center btn-wrapper mt-4">
+                    <a target="_blank" rel="noopener" href="https://twitter.com/creativetim"
+                       class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Follow us">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a target="_blank" rel="noopener" href="https://www.facebook.com/creativetim"
+                       class="btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Like us">
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a target="_blank" rel="noopener" href="https://dribbble.com/creativetim"
+                       class="btn btn-neutral btn-icon-only btn-instagram btn-lg btn-round" data-toggle="tooltip"
+                       data-original-title="Follow us">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                    <a target="_blank" rel="noopener" href="https://github.com/creativetimofficial"
+                       class="btn btn-neutral btn-icon-only btn-linkedin btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Star on Github">
+                        <i class="fa fa-linkedin"></i>
+                    </a>
+                    <a target="_blank" rel="noopener" href="https://github.com/creativetimofficial"
+                       class="btn btn-neutral btn-icon-only btn-telegram btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Star on Github">
+                        <i class="fa fa-telegram"></i>
+                    </a>
+                    <a target="_blank" rel="noopener" href="https://github.com/creativetimofficial"
+                       class="btn btn-neutral btn-icon-only btn-youtube btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Star on Github">
+                        <i class="fa fa-youtube"></i>
+                    </a>
+                </div>
+            </div>
+            <hr>
+            <div class="row align-items-center justify-content-md-between">
+                <div class="col-md-6">
+                    <div class="copyright">
+                        <!-- &copy; {{year}} -->
+                        <p>© 2018, STEMpower. All Rights Reserved</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <ul class="nav nav-footer justify-content-end">
+                        <li class="nav-item">
+                            <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener"><i class="fa fa-phone"></i> + 1 703-463-4468</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank" rel="noopener"><i class="fa fa-envelope-o"></i> info@stempower.org
+                                Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6 mb-5 mb-lg-0">
-          <div class="card card-lift--hover shadow border-0">
-            <router-link title="Profile Page" to="/profile">
-              <img class="card-img" v-lazy="'img/theme/profile.jpg'">
-            </router-link>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="row row-grid align-items-center my-md">
-        <div class="col-lg-6">
-          <h3 class="text-primary font-weight-light mb-2">
-            Thank you for supporting us!
-          </h3>
-          <h4 class="mb-0 font-weight-light">
-            Let's get in touch on any of these platforms.
-          </h4>
-        </div>
-        <div class="col-lg-6 text-lg-center btn-wrapper">
-          <a class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg" data-original-title="Follow us"
-             data-toggle="tooltip"
-             href="https://twitter.com/creativetim" rel="noopener"
-             target="_blank"
-          >
-            <i class="fa fa-twitter"/>
-          </a>
-          <a class="btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg" data-original-title="Like us"
-             data-toggle="tooltip"
-             href="https://www.facebook.com/creativetim" rel="noopener"
-             target="_blank"
-          >
-            <i class="fa fa-facebook-square"/>
-          </a>
-          <a class="btn btn-neutral btn-icon-only btn-dribbble btn-lg btn-round" data-original-title="Follow us"
-             data-toggle="tooltip"
-             href="https://dribbble.com/creativetim" rel="noopener"
-             target="_blank"
-          >
-            <i class="fa fa-dribbble"/>
-          </a>
-          <a class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg" data-original-title="Star on Github"
-             data-toggle="tooltip"
-             href="https://github.com/creativetimofficial" rel="noopener"
-             target="_blank"
-          >
-            <i class="fa fa-github"/>
-          </a>
-        </div>
-      </div>
-      <hr>
-      <div class="row align-items-center justify-content-md-between">
-        <div class="col-md-6">
-          <div class="copyright">
-            &copy; {{ year }}
-            <a href="https://www.creative-tim.com" rel="noopener" target="_blank">Creative Tim</a> & <a
-            href="https://www.binarcode.com" rel="noopener" target="_blank"
-          >Binar Code</a>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <ul class="nav nav-footer justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com" rel="noopener" target="_blank">Creative Tim</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/presentation" rel="noopener" target="_blank">About
-                Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="http://blog.creative-tim.com" rel="noopener" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link"
-                 href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md" rel="noopener"
-                 target="_blank"
-              >MIT License</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
+    </footer>
 </template>
 <script>
-  export default {
-    name: 'AppFooter',
-    data () {
-      return {
-        year: new Date().getFullYear()
-      }
+export default {
+  name: 'app-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
     }
   }
+};
 </script>
 <style>
 </style>
